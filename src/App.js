c3e4be68622ec43af6c9e27d8df932c0493528ee
Sujeet kushwaha <sujeetgroups@gmail.com>
2023-06-29 10:40:20 +0530

    auto logout when token expired

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Header from './components/header/Header';
 import PageRoutes from './routes/PageRoutes';
 import { useEffect } from 'react';
+import { decodeToken } from 'react-jwt';
+import { logout } from './app/features/authSlice';
 
 function App() {
   const {color} = useSelector(state => state.theme)
+  const token = useSelector(state => state.auth.token)
+  const dispatch = useDispatch();
+
+
+  const isTokenExpired = (token) => {
+    if (!token) return true
+    const currentTime = Math.floor(Date.now()/1000);
+    const decodedToken = decodeToken(token)
+    return currentTime>decodedToken.exp
+  }
+
+  useEffect(() => {
+    const checkTokenExpiration = () => {
+      if (isTokenExpired(token)){
+        dispatch(logout())
+      }
+    }
+    const checkTokenExpirationInterval = setInterval(checkTokenExpiration, 1000);
+
+    return () => clearInterval(checkTokenExpirationInterval);
+  },[dispatch, token])
 
   useEffect(() => {
     document.body.style.backgroundColor = color;
